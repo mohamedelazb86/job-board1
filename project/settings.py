4c8e1730c78c5abf5829f19ee987cbc7cd0c7a0f
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2025-08-30 11:33:20 +0200

    create superuser

--- project/settings.py
+++ project/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT='/static/'
+
+
+# MEDIA
+MEDIA_URL='/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
